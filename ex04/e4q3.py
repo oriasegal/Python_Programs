4b3b56c115c351c683b7230d4b8af268d93b7a06
oriasegal <73060217+oriasegal@users.noreply.github.com>
2021-02-16 10:32:24 +0200

    ex04

---
+++ ex04/e4q3.py
@@ -0,0 +1,93 @@
+"""
+Oria Segal 209338193
+Exercise 4, question 3
+This program is getting a list of lists from the user and by using recursive functions can sort them or print
+them in reverse.
+"""
+
+
+def bubblesort(l):
+    def bubblesortrec(l, i, j, n):
+        if j == n:
+            i = i + 1
+            j = 0
+        if i == n:
+            return
+        if l[i] < l[j]:
+            temp = l[j]
+            l[j] = l[i]
+            l[i] = temp
+
+            bubblesortrec(l, i, j + 1, n)
+        else:
+            bubblesortrec(l, i, j + 1, n)
+
+        return l
+
+    l = bubblesortrec(l, 0, 0, len(l))
+    return l
+
+
+def sortedzip(l):
+    s = []
+
+    def sortedziprec(i, l, s):
+        if i == len(l):
+            return s
+        else:
+            s.append(bubblesort(l[i]))
+            return sortedziprec(i + 1, l, s)
+
+    s = sortedziprec(0, l, [])
+    zippedS = list(zip(*s))
+    return zippedS
+
+
+def reverse(l):
+    return l[::-1]
+
+
+def reversedzip(l):
+    s = []
+
+    def reversedziprec(i, l, s):
+        if i == len(l):
+            return s
+        else:
+            s.append(reverse(l[i]))
+            return reversedziprec(i + 1, l, s)
+
+    s = reversedziprec(0, l, [])
+    reversedS = list(zip(*s))
+    return reversedS
+
+
+def funczip(func, l):
+    if func == sortedzip:
+        print("The original list is:")
+        print(l)
+        zippedL = sortedzip(l)
+        print("The sorted and zipped list is:")
+        print(zippedL)
+
+        return
+
+    if func == reversedzip:
+        print("The original list is:")
+        print(l)
+        reversedL = reversedzip(l)
+        print("The reversed and zipped list is:")
+        print(reversedL)
+
+        return
+
+
+l = eval(input("Enter a list of lists: "))
+functions = {'sortedzip': sortedzip, 'reversedzip': reversedzip}
+f = input("Enter the function you want to operate on your list\nTo sort and zip enter sortedzip\nTo reverse and zip enter reversedzip\n")
+func = functions[f]
+funczip(func, l)
+print(l)
+
+
+# l = [['c', 'b', 'a', 'z'], [5, 6, 10, 1], [20, 19, 18, 17]]
---
+++ ex04/e4q4.py
@@ -0,0 +1,61 @@
+"""
+Oria Segal 209338193
+Exercise 4, question 4
+This program is creating an N lines long poem by using two recursive functions.
+"""
+import random
+
+objects = ("Roni ", "Max ", "A chair ", "A bottle ")
+verbs = ("eats ", "plays ", "buys ", "opens ")
+adverbs = ("slowly ", "tomorrow ", "now ", "soon ", "suddenly ")
+adjectives = ("tall ", "small ", "big ", "red ", "blue ")
+animateObjects = ("oranges", "friends", "lake", "food", "trees")
+
+
+def crPoem(N):
+    # a function that creates a poem by using a recursive sub- function
+    l = []
+
+    def cp(N, l):
+        # a recursive sub- function that creates a poem (a tuple of strings)
+        # by choosing a random sentence from global options
+        if N == 0:
+            return l
+        else:
+            obj = str(random.choice(objects))
+            verb = str(random.choice(verbs))
+            adverb = str(random.choice(adverbs))
+            adj = str(random.choice(adjectives))
+            aniObj = str(random.choice(animateObjects))
+
+            l.append(obj + verb + adverb + adj + aniObj)
+
+            return cp(N - 1, l)
+
+    poem = tuple(cp(N, l))
+    return poem
+
+
+def theHumbletPoet():
+    # a function that is asking for a positive number, checks if it's correct
+    # and uses a recursive sub- function to print the poem
+    N = int(input("Enter the number of lines for your poem: "))
+
+    while N < 0:  # checks if the value entered is correct
+        print("The value entered is incorrect. Please enter a new value.")
+        N = int(input())
+
+    poem = crPoem(N)  # calls the recursive function to create a poem
+
+    def printPoem(poem):
+        # prints the poem recursively
+        if len(poem) == 0:
+            return
+        else:
+            r = list(poem)
+            print(r.pop(0))
+            return printPoem(r)
+
+    printPoem(poem)
+
+theHumbletPoet()
